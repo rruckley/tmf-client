44acab5672548bd18596751780e6d32a3066d52b
Ryan <rruckley@gmail.com>
2024-02-04 21:42:30 +1100

    Basic struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct TMFClient {
+    host : String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl TMFClient {
+    pub fn new(host : impl Into<String>) -> TMFClient {
+        TMFClient {
+            host : host.into(),
+        }
     }
 }
