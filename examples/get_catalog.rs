6e538d8f4bfc24e97230ce3b7fd289460dbe92b4
Ryan Ruckley <rruckley@gmail.com>
2024-02-06 12:36:27 +1100

    expanded example

--- examples/get_catalog.rs
+++ examples/get_catalog.rs
@@ -1,10 +1,22 @@
 //! Get Catalog Example
 
-use tmf_client::TMFClient;
+use tmf_client::{TMFClient,QueryOptions};
 
 fn main() {
     let mut client = TMFClient::new("http://localhost:8080");
-    let tmf = client.tmf620();
-
-    dbg!(tmf);
+    let filter = QueryOptions::default()
+        .limit(5)
+        .offset(0);
+    let tmf = client.tmf620()
+        .catalog()
+        .list(Some(filter));
+    match tmf {
+        Ok(r) => {
+            // It worked
+            println!("It worked: {} items",r.len())
+        },
+        Err(e) => {
+            println!("Error: {:?}",e)
+        },
+    }
 }
