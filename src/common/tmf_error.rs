ba3de6660123d9bffea9f0a42244463af77dbe3a
Ryan Ruckley <rruckley@gmail.com>
2024-02-06 15:45:16 +1100

    tests

--- src/common/tmf_error.rs
+++ src/common/tmf_error.rs
@@ -1,9 +1,9 @@
 
 //! Error Module
 
-#[derive(Debug)]
+#[derive(Debug,Default)]
 pub struct TMFError {
-    message : String,
+    pub message : String,
 }
 
 impl From<&str> for TMFError {
@@ -19,5 +19,32 @@ impl From<reqwest::Error> for TMFError {
         TMFError {
             message : value.to_string(),
         }
+    }
+}
+
+impl From<TMFError> for String {
+    fn from(value: TMFError) -> Self {
+        format!("TMFError: {}",value.message)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_error_from_str() {
+        let err = TMFError::from("ThisIsAnError");
+
+        assert_eq!(err.message,"ThisIsAnError".to_string());
+    }
+
+    #[test]
+    fn test_string_from_error() {
+        let err = TMFError {
+            message : "ThisIsAnError".into(),
+        };
+
+        let s : String = err.into();
+        assert_eq!(s,"TMFError: ThisIsAnError".to_string());
     }
 }
