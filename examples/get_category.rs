ae0c930be3320aa873eb0b524c5e46a02465e94a
Ryan Ruckley <rruckley@gmail.com>
2024-02-06 13:55:05 +1100

    Category example

---
+++ examples/get_category.rs
@@ -0,0 +1,26 @@
+//! Get Category Example
+
+use tmf_client::{TMFClient,QueryOptions};
+use tmflib::HasName;
+
+fn main() {
+    // Get a list of categories from TMF620
+    let mut client = TMFClient::new("http://localhost:8000");
+    let filter = QueryOptions::default()
+        .limit(1)
+        .offset(0);
+    let tmf = client.tmf620()
+        .category()
+        .list(Some(filter));
+    match tmf {
+        Ok(r) => {
+            // It worked
+            for c in r {
+                println!("Entry: {}",c.get_name())
+            }
+        },
+        Err(e) => {
+            println!("Error: {:?}",e)
+        },
+    }
+}
