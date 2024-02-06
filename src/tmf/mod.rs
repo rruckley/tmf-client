f8b96abe55d59d971a53438793b47b600670ca7a
Ryan Ruckley <rruckley@gmail.com>
2024-02-06 15:46:25 +1100

    filter for id

--- src/tmf/mod.rs
+++ src/tmf/mod.rs
@@ -4,7 +4,7 @@
 use crate::QueryOptions;
 use crate::common::tmf_error::TMFError;
 use tmflib::{HasId,HasName,Uri};
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde::de::DeserializeOwned;
 
 pub mod tmf620;
 pub mod tmf622;
@@ -12,7 +12,7 @@ pub mod tmf622;
 /// Make API call to retrieve a single TMF object
 pub fn get_tmf<T : HasId + HasName + DeserializeOwned>(host: Uri, id : String) -> Result<Vec<T>,TMFError> {
     // Return results
-    let url = format!("{}{}",host,T::get_class_href());
+    let url = format!("{}{}/{}",host,T::get_class_href(),id);
     let catalogs = reqwest::blocking::get(url)?.text()?;
     let output : Vec<T> = serde_json::from_str(catalogs.as_str()).unwrap();
     Ok(output)
@@ -21,7 +21,10 @@ pub fn get_tmf<T : HasId + HasName + DeserializeOwned>(host: Uri, id : String) -
 /// Make API call to retrieve a set of TMF objects according to filter
 pub fn list_tmf<T : HasId + HasName + DeserializeOwned>(host: Uri, filter : Option<QueryOptions>) -> Result<Vec<T>,TMFError> {
     // Return results
-    let filter : String = filter.unwrap().into();
+    let filter = match filter {
+        Some(f) => f.into(),
+        None => String::default(),
+    };
     let url = format!("{}{}?{}",host,T::get_class_href(),filter);
     println!("Filter: {}",filter);
     let catalogs = reqwest::blocking::get(url)?.text()?;
