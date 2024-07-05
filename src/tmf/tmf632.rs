dbd259fe5aa2bce988268b316b49a2e4cfcc919a
Ryan Ruckley <rruckley@gmail.com>
2024-07-05 14:53:30 +1000

    imp org operations

--- src/tmf/tmf632.rs
+++ src/tmf/tmf632.rs
@@ -1,6 +1,7 @@
 //! TMF632 Party
 //! 
 
+use tmflib::tmf632::organization_v4::Organization;
 use tmflib::{Uri,HasId,HasName};
 use tmflib::tmf632::individual_v4::Individual;
 use crate::Operations;
@@ -50,6 +51,26 @@ impl TMF632Organization {
     /// Create a new instance of Product Order API Object
     pub fn new(host : Uri) -> TMF632Organization {
         TMF632Organization{ host }
+    }
+}
+
+impl Operations for TMF632Organization {
+    type TMF = Organization;
+
+    fn create(&self, item : Self::TMF) -> Result<Self::TMF,TMFError> {
+        create_tmf(self.host.clone(), item)    
+    }
+    fn delete(&self, id : impl Into<String>) -> Result<Self::TMF,TMFError> {
+        Err(TMFError::from("Not implemented"))      
+    }
+    fn get(&self, id : impl Into<String>) -> Result<Vec<Self::TMF>,TMFError> {
+        get_tmf(self.host.clone(),id.into())    
+    }
+    fn list(&self, filter : Option<crate::QueryOptions>) -> Result<Vec<Self::TMF>,TMFError> {
+        list_tmf(self.host.clone(),filter)    
+    }
+    fn update(&self, id : impl Into<String>, patch : Self::TMF) -> Result<Self::TMF,TMFError> {
+        Err(TMFError::from("Not implemented"))    
     }
 }
 
