12a2b82abe5935a687a3f943c53976329ce18e06
Ryan Ruckley <rruckley@gmail.com>
2024-07-05 14:53:07 +1000

    add org example

---
+++ examples/get_organization.rs
@@ -0,0 +1,19 @@
+//! Get Organization Example
+
+use tmf_client::common::tmf_error::TMFError;
+use tmf_client::{Operations, TMFClient};
+use tmflib::{HasId,HasName};
+
+fn main() -> Result<(),TMFError> {
+
+    let organizations = TMFClient::new("http://localhost:8000")
+        .tmf632()
+        .organization()
+        .list(None)?;
+
+    for o in organizations {
+        println!("Name: {} , Id: {}",o.get_name(),o.get_id());
+    }
+
+    Ok(())
+}
