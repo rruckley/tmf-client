42af687038b6fa3b324eea75184e4229ab864d43
Ryan Ruckley <rruckley@gmail.com>
2024-10-25 13:34:23 +1100

    conditional dep

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,9 @@ use tmf::tmf632::TMF632;
 
 use tmflib::HasId;
 
+#[cfg(feature = "oauth2")]
+use oauth2::*;
+
 /// Fields for filtering output
 #[derive(Clone, Default, Debug)]
 pub struct QueryOptions {
