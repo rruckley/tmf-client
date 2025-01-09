1241f5125965ee290ea00c8f212c4160f8fd6358
Ryan Ruckley <rruckley@gmail.com>
2025-01-10 10:58:56 +1100

    feat: New example

---
+++ examples/create_quote.rs
@@ -0,0 +1,20 @@
+//! Create Quote Example
+//! 
+use tmf_client::{Operations, TMFClient};
+use tmf_client::common::tmf_error::TMFError;
+use tmflib::tmf648::quote::Quote;
+
+
+fn main() -> Result<(),TMFError> {
+    let quote = Quote::new();
+
+    let new_quote = TMFClient::new("http://localhost:8001")
+        .tmf648()
+        .quote()
+        .create(quote);
+
+
+    dbg!(new_quote);
+
+    Ok(())
+}
