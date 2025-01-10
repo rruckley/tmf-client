7d5d44f43c63ccd6b0442fdaf22338a9b8c501bc
Ryan Ruckley <rruckley@gmail.com>
2025-01-10 13:48:49 +1100

    feat: New TMF modules

--- src/tmf/tmf633.rs
+++ src/tmf/tmf633.rs
@@ -1,8 +1,76 @@
 //! TMF633 Module
 
 use tmflib::Uri;
+use tmflib::tmf633::service_candidate::ServiceCandidate;
+use tmflib::tmf633::service_specification::ServiceSpecification;
 
+use crate::common::tmf_error::TMFError;
 use crate::{Operations,HasNew};
+use super::{
+    create_tmf,
+    get_tmf,
+    list_tmf,
+};
+
+/// ServiceCandidate API Object
+pub struct TMF633Candidate {
+    host: Uri,
+}
+
+impl Operations for TMF633Candidate {
+    type TMF = ServiceCandidate;
+
+    fn create(&self, item : Self::TMF) -> Result<Self::TMF,TMFError> {
+        create_tmf(self.host.clone(),item)    
+    }
+    fn delete(&self, _id : impl Into<String>) -> Result<Self::TMF,TMFError> {
+        Err(TMFError::from("Not implemented")) 
+    }
+    fn get(&self, id : impl Into<String>) -> Result<Vec<Self::TMF>,TMFError> {
+        get_tmf(self.host.clone(),id.into())  
+    }
+    fn list(&self, filter : Option<crate::QueryOptions>) -> Result<Vec<Self::TMF>,TMFError> {
+        list_tmf(self.host.clone(),filter)  
+    }
+    fn update(&self, _id : impl Into<String>, _patch : Self::TMF) -> Result<Self::TMF,TMFError> {
+        Err(TMFError::from("Not implemented")) 
+    }    
+}
+
+/// ServiceCatalog API Object
+pub struct TMF633Catalog {
+    host: Uri,
+}
+
+/// ServiceCategory API Object
+pub struct TMF633Category {
+    host: Uri,
+}
+
+/// ServiceSpecification API Object
+pub struct TMF633Specification {
+    host: Uri,
+}
+
+impl Operations for TMF633Specification {
+    type TMF = ServiceSpecification;
+
+    fn create(&self, item : Self::TMF) -> Result<Self::TMF,TMFError> {
+        create_tmf(self.host.clone(),item)    
+    }
+    fn delete(&self, _id : impl Into<String>) -> Result<Self::TMF,TMFError> {
+        Err(TMFError::from("Not implemented"))
+    }
+    fn get(&self, id : impl Into<String>) -> Result<Vec<Self::TMF>,TMFError> {
+        get_tmf(self.host.clone(),id.into())  
+    }
+    fn list(&self, filter : Option<crate::QueryOptions>) -> Result<Vec<Self::TMF>,TMFError> {
+        list_tmf(self.host.clone(),filter)  
+    }
+    fn update(&self, _id : impl Into<String>, _patch : Self::TMF) -> Result<Self::TMF,TMFError> {
+        Err(TMFError::from("Not implemented")) 
+    }        
+}
 
 /// Product Catalogue API
 #[derive(Clone,Default,Debug)]
@@ -15,5 +83,27 @@ impl HasNew<TMF633> for TMF633 {
         TMF633 {
             host
         }
+    }
+}
+
+impl TMF633 {
+    /// Create ServiceCandidate API object
+    pub fn candidate(&self) -> TMF633Candidate {
+        TMF633Candidate { host: self.host.clone() }
+    }
+
+    /// Create ServiceCatalog API object
+    pub fn catalog(&self) -> TMF633Catalog {
+        TMF633Catalog { host: self.host.clone()}
+    }
+
+    /// Create ServiceCategory API Object
+    pub fn category(&self) -> TMF633Category {
+        TMF633Category { host: self.host.clone() }
+    }
+
+    /// Create ServiceSpecification API Object
+    pub fn specification(&self) -> TMF633Specification {
+        TMF633Specification { host: self.host.clone() }
     }
 }
--- src/tmf/tmf674.rs
+++ src/tmf/tmf674.rs
@@ -1,7 +1,40 @@
 //! TMF674 Module
 
 use tmflib::Uri;
+use tmflib::tmf674::geographic_site_v4::GeographicSite;
+
+use crate::common::tmf_error::TMFError;
 use crate::{Operations,HasNew};
+use super::{
+    create_tmf,
+    get_tmf,
+    list_tmf,
+};
+
+/// TMF674 GeographicSite API Object
+pub struct TMF674GeographicSite {
+    host : Uri,
+}
+
+impl Operations for TMF674GeographicSite {
+    type TMF = GeographicSite;
+    
+    fn create(&self, item : Self::TMF) -> Result<Self::TMF,TMFError> {
+        create_tmf(self.host.clone(),item)    
+    }
+    fn delete(&self, _id : impl Into<String>) -> Result<Self::TMF,TMFError> {
+        Err(TMFError::from("Not implemented")) 
+    }
+    fn get(&self, id : impl Into<String>) -> Result<Vec<Self::TMF>,TMFError> {
+        get_tmf(self.host.clone(),id.into())  
+    }
+    fn list(&self, filter : Option<crate::QueryOptions>) -> Result<Vec<Self::TMF>,TMFError> {
+        list_tmf(self.host.clone(),filter)  
+    }
+    fn update(&self, _id : impl Into<String>, _patch : Self::TMF) -> Result<Self::TMF,TMFError> {
+        Err(TMFError::from("Not implemented")) 
+    }
+}
 
 /// Product Catalogue API
 #[derive(Clone,Default,Debug)]
@@ -13,6 +46,15 @@ impl HasNew<TMF674> for TMF674 {
     fn new(host : Uri) -> TMF674 {
         TMF674 {
             host
+        }
+    }
+}
+
+impl TMF674 {
+    /// Provide a GeographicSite API object
+    pub fn site(&self) -> TMF674GeographicSite {
+        TMF674GeographicSite {
+            host: self.host.clone(),
         }
     }
 }
