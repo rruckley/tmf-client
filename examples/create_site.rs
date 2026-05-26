80396411c1d2d72e0ec1512ca502796646a38a23
Ryan Ruckley <ryan.ruckley@optus.com.au>
2026-05-27 08:28:19 +1000

    fix: Add description

--- examples/create_site.rs
+++ examples/create_site.rs
@@ -5,11 +5,13 @@ use tmf_client::common::tmf_error::TMFError;
 use tmf_client::{BlockingOperations, TMFClient};
 #[cfg(feature = "tmf674")]
 use tmflib::tmf674::geographic_site_v4::GeographicSite;
+use tmflib::HasDescription;
 
 fn main() -> Result<(), TMFError> {
     #[cfg(feature = "tmf674")]
     {
-        let site = GeographicSite::new("Example Bad Site");
+        let site = GeographicSite::new("Example Bad Site")
+            .description("Just a sample data payload for testing tmf-client library");
 
         let new_site = TMFClient::new("https://localhost", None)
             .tmf674()
